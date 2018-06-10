363bd48e9d57d7ddb70e2c9361976c45e5578beb
wangyongalive <1042348844@qq.com>
2018-06-10 21:34:39 +0800

    学生管理系统

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var db = require('./../db.js');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    var mysqlQuery = 'SELECT * FROM student'
+    db.DBConnection.query(mysqlQuery,function(err,rows,fields){
+        if(err){
+            console.log(err);
+            return;
+        }
+        // user模板
+        res.render('user', {students:rows})
+    });
+});
+
+module.exports = router;
